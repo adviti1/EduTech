90456c967f4d5e85604b3d9e7848721396aad45c
adviti1 <advitigangwar123@gmail.com>
2024-07-26 18:07:21 +0530

    background and logo

---
+++ edu-tech/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="logo"></div>
+    {/* <div className="search-container">
+          <input type="text" placeholder="Search.." name="search" />
+          <button type="submit"><i className="fa fa-search"></i></button>
+      </div> */}
+      <div className="part1">
+        <div className="card1">
+          <h2>Start Your Learning Journey Today</h2>
+          <p>Sign Up for Free!</p>
+          <button>Sign Up</button>
+        </div>
+        {/* <div className="card">
+          <h2>Master Coding for Free: Dive into Courses for Every Programming Language!</h2>
+          <div className="language-box">
+            <p>Java</p>
+          </div>
+        </div>
+        <div className="card">
+          <h2>Unlock Your Potential:</h2>
+          <p>Explore a wide range of interactive courses designed to enhance your skills and knowledge</p>
+        </div> */}
+      </div>
+      </div>
+  );
+}
+
+export default App;
